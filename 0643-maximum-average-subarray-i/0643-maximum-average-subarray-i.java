7784e794e074a72f71d0ced6e35ff197fa450306
Aryan raj <119423612+aryanrajok@users.noreply.github.com>
2026-08-11 19:24:35 +0530

    Time: 3 ms (73.44%), Space: 69.6 MB (63.55%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -6,9 +6,10 @@ class Solution {
         }
         
         int maxSum = sum;
+        
         for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];   // slide the window
-            maxSum = Math.max(maxSum, sum); // track best sum
+            sum = sum + nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
         }
         
         return (double) maxSum / k;
