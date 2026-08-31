624f798bbc1e80cd51e61d7ff9f9bd15ee801366
Aryan raj <119423612+aryanrajok@users.noreply.github.com>
2026-09-01 03:02:02 +0530

    Time: 2 ms (95.63%), Space: 48.2 MB (79.73%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -7,15 +7,14 @@ class Solution {
             int sum = numbers[left] + numbers[right];
 
             if (sum == target) {
-                // Return 1-based indices
                 return new int[]{left + 1, right + 1};
             } else if (sum < target) {
                 left++;
             } else {
                 right--;
             }
-        }
+        } 
 
-        return new int[]{}; // Problem guarantees a solution exists.
-    }
-}
+        return new int[]{-1, -1}; 
+    } 
+} 
